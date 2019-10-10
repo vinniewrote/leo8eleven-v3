54d4676c4d06e4f4c84d62c72a10b35066d04ecd
Vizente Freeman <vizente@leo8eleven.com>
2019-10-09 23:15:51 -0500

    fix for image and spacing issue

--- src/components/Salutation.js
+++ src/components/Salutation.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import headshotimg from '../headshot-sm.png';
 import Connect from './ConnectHub';
 import Header from './Header';
